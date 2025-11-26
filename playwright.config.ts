b0777010c8c82d2e63e55fa54503356a07f27524
Gidon Avivi <113175524+Gidon31@users.noreply.github.com>
2025-11-27 00:59:53 +0200

    fix report issues

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,11 +3,14 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   use: {
     baseURL: 'https://atid.store/',
-    headless: true
+    headless: true,
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
   },
-  
   reporter: [
     ['list'],
-    ['allure-playwright']
-  ],
+    ['allure-playwright'],
+    ['html', { open: 'never' }]
+  ]
 });
